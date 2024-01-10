8e6ddd9d3b2194a9938a146d3e2e23e7a48f2b81
yuliyahrudzko <yuliya.grudko@mail.ru>
2024-01-10 16:05:02 +0300

    add logger

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,33 +1,31 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
+  // Look for test files in the "tests" directory, relative to this configuration file.
   testDir: './tests',
-  /* Run tests in files in parallel */
+  // Run tests in files in parallel
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  // Fail the build on CI if you accidentally left test.only in the source code. 
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
+  // Retry on CI only 
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
+  // Opt out of parallel tests on CI. 
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  // Reporter to use. See https://playwright.dev/docs/test-reporters 
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
+    // Base URL to use in actions like `await page.goto('/')`. 
     // baseURL: 'http://127.0.0.1:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    // Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer 
     trace: 'on-first-retry',
+    launchOptions: {
+      logger: {
+        isEnabled: (name, severity) => true,
+        log: (name, severity, message) => console.log(`${name} ${message}`)
+      }
+    }
   },
 
   /* Configure projects for major browsers */
